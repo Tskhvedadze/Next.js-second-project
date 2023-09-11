1d99dfa1172da55bfaa4f63bc628cd4c2439decc
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-11 13:19:15 +0400

    comlete

--- pages/lastSales.js
+++ pages/lastSales.js
@@ -1,25 +1,38 @@
 import useSWR from "swr";
 import { fetcher } from "../fetcher";
 
-function LastSalesPage() {
-  const { data, error, isLoading } = useSWR(
-    "https://nextjs-79a04-default-rtdb.firebaseio.com/sales.json",
-    fetcher
-  );
+const URL = "https://nextjs-79a04-default-rtdb.firebaseio.com/sales.json";
 
-  if (error) return <p>Failed to fetch</p>;
+function LastSalesPage({ sales }) {
+  const { data, error, isLoading } = useSWR(URL, fetcher);
 
-  if (!data) return <p>Loading...</p>;
+  //   if (error) return <p>Failed to fetch</p>;
+
+  //   if (!data) return <p>Loading...</p>;
 
   return (
     <ul>
-      {data.map((sale) => (
+      {sales.map((sale) => (
         <li key={sale.id}>
           {sale.username} - <span style={{ color: "green" }}>$</span>
           {sale.volume}
         </li>
       ))}
     </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return fetch(URL).then((response) =>
+    response.json().then((data) => {
+      const transformedData = [];
+      for (const key in data) {
+        transformedData.push(data[key]);
+      }
+      return {
+        props: { sales: transformedData },
+      };
+    })
   );
 }
 
